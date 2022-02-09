fa0055df1ecc04cea9c3307c79140640076bf93f
Alejandro Benitez <alejoxpi@gmail.com>
2022-02-09 01:47:00 -0500

    Act

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Cadena de conexion mongo: \n');
+  res.end('IP Privada: mongodb://mongoadmin:8Tj38d9BFhqDhy3x@3.133.124.221:27017/?authSource=admin \n IP Publica: mongodb://mongoadmin:8Tj38d9BFhqDhy3x@3.133.124.221:27017/?authSource=admin \n');
 });
 
 server.listen(port, hostname, () => {
